594b61b83d387c4ab27315291c265cd5fcd80aa0
stupiditygenuine <gaon070410@gmail.com>
2022-02-19 01:09:52 -0600

    testing continued

---
+++ script.js
@@ -0,0 +1,16 @@
+//only comment whys and hows. dont explain what the code does
+//var rules: Cannot be a reserved keyword. 
+//Be meaningful. Cannot start with a number. 
+//Cannot contain space or hyphen. 
+//declare each variable in a seperate line
+//first word should be lowercase and then all words after that starts with a capitalized letter, firstTest
+console.log('script works');
+//main script
+//name test
+let name = 'Goonie';
+let lastName = 'Oh';
+console.log(name, lastName);
+//constant test
+const inflationRate = 0.3;
+inflationRate = 1;
+console.log(interestRate)
